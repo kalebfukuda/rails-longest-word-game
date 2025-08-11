d84463a26432679af84db3032de164856eb5133f
kaleb fukuda <kalebfukudajp@gmail.com>
2025-08-11 16:10:37 +0900

    Changed method from get to post

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   root "games#new"
 
   get 'new', to: 'games#new', as: :new
-  get 'score', to: 'games#score', as: :score
+  post 'score', to: 'games#score', as: :score
 end
